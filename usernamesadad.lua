2f878cb379c4eff4d1ceeecb251504a61e9c1aa9
QwertyKz <94755748+QwertyKz@users.noreply.github.com>
2022-05-29 10:34:11 -0700

    Add files via upload

---
+++ usernamesadad.lua
@@ -0,0 +1,79 @@
+local username_true = {}
+local username = globals.get_username()
+username_true['rings1337'] = true
+username_true['Ruzule'] = true
+username_true['ItapingU'] = true
+username_true['Remiii'] = true
+username_true['DariusQIZ'] = true
+username_true['Prync'] = true
+username_true['Cabrons'] = true
+username_true['JNHVH'] = true
+username_true['thunderzeraa'] = true
+username_true['Jdarey'] = true
+username_true['xFr3sH'] = true
+username_true['K69'] = true
+username_true['Mikuu'] = true
+username_true['Retadz'] = true
+username_true['Ignacy'] = true
+username_true['DeadEye'] = true
+username_true['Bearlyeight75'] = true
+username_true['Jason08910'] = true
+username_true['Raduumac'] = true
+username_true['REZKO'] = true
+username_true['EliteX'] = true
+username_true['KornyBuns'] = true
+username_true['Andy22'] = true
+username_true['Barbu1989999'] = true
+username_true['bighandsoon'] = true
+username_true['eugen'] = true
+username_true['ph1losophy'] = true
+username_true['beadyy21'] = true
+username_true['sN1p3z'] = true
+username_true['S2W'] = true
+username_true['Netflix34'] = true
+username_true['CeZz'] = true
+username_true['Sebighei'] = true
+username_true['Cookie52'] = true
+username_true['LaZe'] = true
+username_true['Kowatt'] = true
+username_true['hayru aslan'] = true
+username_true['w0psy'] = true
+username_true['AWOKADO'] = true
+username_true['Lugaw'] = true
+username_true['AlexEzz12'] = true
+username_true['Nexiss'] = true
+username_true['Pndd'] = true
+username_true['ClownK1NG'] = true
+username_true['Dorel12'] = true
+username_true['Exapog'] = true
+username_true['Carleel'] = true
+username_true['buceaking'] = true
+username_true['HKhang'] = true
+username_true['Bucket'] = true
+username_true['bortocel'] = true
+username_true['Taik'] = true
+username_true['Tap'] = true
+username_true['david ENZO'] = true
+username_true['ZaXeN1338'] = true
+username_true['tromoslav'] = true
+username_true['Nunne'] = true
+username_true['ONyxNato'] = true
+username_true['Forever_3210'] = true
+username_true['Victoriaaaa'] = true
+username_true['DandaPanda'] = true
+username_true['QuezzyxRegele'] = true
+username_true['Rob Jobs'] = true
+username_true['saolong1337'] = true
+username_true['SzabiEz'] = true
+--DandaPanda
+
+
+local function sendtrue()
+    if username_true[username] == true then
+      return true
+    else
+      return false
+    end
+end
+
+return sendtrue()
